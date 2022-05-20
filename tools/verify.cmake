af931ef98811f7259be72f4848551d853631ec91
ralberd <ralberd@sandia.gov>
2022-05-20 10:06:35 -0600

    adding mass properties test

--- tools/verify.cmake
+++ tools/verify.cmake
@@ -1,8 +1,10 @@
 #  run verification problem
-set( TEST_COMMAND "${ANALYZE_EXECUTABLE} --output-viz=${OUTPUT_DATA} --input-config=${CONFIG_FILE}" )
-execute_process(COMMAND bash "-c" "${TEST_COMMAND}" RESULT_VARIABLE HAD_ERROR)
-if (HAD_ERROR)
-  message(FATAL_ERROR "FAILED: ${TEST_COMMAND}")
+if (ANALYZE_EXECUTABLE)
+  set( TEST_COMMAND "${ANALYZE_EXECUTABLE} --output-viz=${OUTPUT_DATA} --input-config=${CONFIG_FILE}" )
+  execute_process(COMMAND bash "-c" "${TEST_COMMAND}" RESULT_VARIABLE HAD_ERROR)
+  if (HAD_ERROR)
+    message(FATAL_ERROR "FAILED: ${TEST_COMMAND}")
+  endif()
 endif()
 
 foreach( varName ${COMPARE_PY} )
